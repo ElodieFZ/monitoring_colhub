14ad2164cb7ce59284c32dcd4f2b3cb8682f1c2a
Elodie Fernandez <elodief@met.no>
2021-10-27 14:21:57 +0200

    reformatting

--- tools/monitoring_logs.py
+++ tools/monitoring_logs.py
@@ -17,15 +17,6 @@ import numpy as np
 logger = logging.getLogger(__name__)
 
 
-def get_success_lines(myfile, pattern='.zip'):
-    ok = []
-    with open(myfile) as f:
-        for line in f:
-            if all(x in line for x in ["successfully downloaded", pattern]):
-                ok.append(line)
-    return ok
-
-
 def get_sensing_time(product_name, sat):
 
     if sat == 'S2DEM':
@@ -45,16 +36,75 @@ def get_sensing_time(product_name, sat):
         return None
 
 
-def products_evicted(myfile):
-    out = 0
+def check_logfile(myfile):
+    synchronized = []
+    downloaded = []
+    ingested = []
+    removed = 0
+    sat = myfile.stem.split('-')[0]
+    if sat == 'S5p':
+        pattern = '.nc'
+    else:
+        pattern = '.zip'
     with open(myfile) as f:
         for line in f:
-            if 'deleted globally' in line:
-                out += 1
-    return out
+            if all(x in line for x in ["successfully downloaded", pattern]):
+                synchronized.append(line)
+            elif all(x in line for x in ["download by", "completed"]):
+                downloaded.append(line)
+            elif 'Ingestion processing complete for product file' in line:
+                ingested.append(line)
+            elif 'deleted globally' in line:
+                removed += 1
+    return synchronized, ingested, downloaded, removed
 
 
-def read_logs_dhus(sat, area, day, logdir):
+def check_synchronized(mylist, sat):
+
+    if len(mylist) == 0:
+        return len(mylist), np.nan, np.nan, np.nan
+
+    log_df = pd.DataFrame(mylist, columns=['all'])
+
+    # Get ingestion date
+    # TODO: create function that includes the exception,
+    # so that we only loose the timeliness information for one or several products instead of the whole df
+    try:
+        log_df['ingestion_date'] = log_df['all'].apply(
+            lambda x: dt.datetime.strptime(x.split('[')[2].split(']')[0], '%Y-%m-%d %H:%M:%S,%f'))
+    except IndexError:
+        logger.error("Problem trying to read timeliness information, so returning NaNs.")
+        return len(mydf), np.nan, np.nan, np.nan
+
+    # Get product name
+    log_df['product_name'] = log_df['all'].apply(
+        lambda x: x.split('[INFO ] Product \'')[1].split('.')[0])
+
+    # Check that product names match the sat name
+    # If not, drop the rows with non-matching product name
+    log_df["sat_name_ok"] = log_df['product_name'].apply(lambda x: x[0:2] == sat[0:2])
+    if not log_df["sat_name_ok"].all():
+        logger.error('Problem with products, some product_names do not match the satellite name.')
+        logger.error('Products that have been ingested: ')
+        logger.error(log_df[~log_df['sat_name_ok']]['product_name'])
+        log_df = log_df.loc[log_df["sat_name_ok"], :]
+
+    # Get sensing date
+    log_df['sensing_date'] = log_df['product_name'].apply(lambda x: get_sensing_time(x, sat))
+
+    # Timeliness = ingestion_date - sensing_date
+    log_df['timeliness'] = log_df['ingestion_date'] - log_df['sensing_date']
+
+    log_df.loc[log_df['timeliness'][::-1].idxmax()][['product_name', 'timeliness']]
+
+    logger.info(f'Min timeliness: {log_df["timeliness"].min()}')
+    logger.info(f'Max timeliness: {log_df["timeliness"].max()}')
+    logger.info(f'Median timeliness: {log_df["timeliness"].median()}')
+
+    return log_df.shape[0], log_df["timeliness"].min(), log_df["timeliness"].max(), log_df["timeliness"].median()
+
+
+def read_logs_dhus(type, sat, area, day, logdir, outfile=None):
     """
     Check dhus logs for one instance / one day
     :param sat: instance name (1 instance = 1 type of sentinel product)
@@ -82,58 +132,26 @@ def read_logs_dhus(sat, area, day, logdir):
     log_day = log_day[0]
 
     logger.debug(f'Checking logfile {log_day}')
-    if sat == 'S5p':
-        successes = get_success_lines(log_day, '.nc')
+    synch_list, down_list, ingested_list, deleted = check_logfile(log_day)
+
+    if 'BE' in type:
+        logger.info(f'{deleted} products successfully evicted.')
+
+        # Check products synchronized (with odata synchronizer)
+        synchronized, timeliness_min, timeliness_max, timeliness_median = check_synchronized(synch_list, sat)
+        logger.info(f'{synchronized} products successfully synchronized (from odata synchronizer).')
+
+        # Check products ingested (with file scanner)
+        ##if sat == 'S1':
+        ##    ingested = check_ingested(ingested_list, sat)
+        ##    logger.info(f'{ingested} products successfully ingested (from file scanner).')
+
+    if 'FE' in type:
+        # Check products downloaded
+        downloaded = check_downloaded(down_list, outfile)
+        logger.info(f'{downloaded} products successfully downloaded.')
+
+    if 'BE' in type:
+        return synchronized, timeliness_min, timeliness_max, timeliness_median, deleted
     else:
-        successes = get_success_lines(log_day)
-    nb_successes = len(successes)
-    logger.info(f'{nb_successes} products successfully downloaded.')
-
-    deleted = products_evicted(log_day)
-    logger.info(f'{deleted} products successfully evicted.')
-
-    if len(successes) == 0:
-        return len(successes), np.nan, np.nan, np.nan, deleted
-
-    log_df = pd.DataFrame(successes, columns=['all'])
-
-    # Get ingestion date
-    # TODO: create function that includes the exception, 
-    # so that we only loose the timeliness information for one or several products instead of the whole df
-    try:
-        log_df['ingestion_date'] = log_df['all'].apply(
-            lambda x: dt.datetime.strptime(x.split('[')[2].split(']')[0], '%Y-%m-%d %H:%M:%S,%f'))
-    except IndexError:
-        logger.error("Problem trying to read timeliness information, so returning NaNs.")
-        return len(successes), np.nan, np.nan, np.nan, deleted
-
-    # Get product name, and sensing date from it
-    log_df['product_name'] = log_df['all'].apply(
-        lambda x: x.split('[INFO ] Product \'')[1].split('.')[0])
-
-    log_df["sat_name_ok"] = log_df['product_name'].apply(lambda x: x[0:2] == sat[0:2])
-    if not log_df["sat_name_ok"].all():
-        logger.error('Problem with products, some product_names do not match the satellite name.')
-        logger.error(f'Actually, only {nb_successes} products successfully downloaded, the rest is ???.')
-        logger.error('Products that have been ingested: ')
-        logger.error(log_df[~log_df['sat_name_ok']]['product_name'])
-
-        log_df = log_df.loc[log_df["sat_name_ok"], :]
-        nb_successes = log_df.shape[0]
-
-        #return np.nan, np.nan, np.nan, np.nan, deleted
-
-    log_df['sensing_date'] = log_df['product_name'].apply(lambda x: get_sensing_time(x, sat))
-
-    # Timeliness = ingestion_date - sensing_date
-    log_df['timeliness'] = log_df['ingestion_date'] - log_df['sensing_date']
-
-    log_df.loc[log_df['timeliness'][::-1].idxmax()][['product_name', 'timeliness']]
-
-    logger.info(f'Min timeliness: {log_df["timeliness"].min()}')
-    logger.info(f'Max timeliness: {log_df["timeliness"].max()}')
-    logger.info(f'Median timeliness: {log_df["timeliness"].median()}')
-
-    return nb_successes, log_df["timeliness"].min(), log_df["timeliness"].max(), log_df["timeliness"].median(), deleted
-
-
+        return None
