71da8d6a50fe06f48f8d51ec401d59890087040f
nbs <elodief@met.no>
2021-11-08 10:28:55 +0000

    Fix paths for nbs user

--- monitoring_dhus_logs.sh
+++ monitoring_dhus_logs.sh
@@ -6,10 +6,10 @@
 #$ -q research-el7.q
 #$ -pe mpi 1
 #$ -j y
-#$ -wd /home/elodief/monitoring_colhub/logs
-#$ -o /home/elodief/monitoring_colhub/logs
+#$ -wd /lustre/storeB/project/NBS2/sentinel/production/NorwAREA/netCDFNBS_work/prod_tmp/logs/ppi
+#$ -o /lustre/storeB/project/NBS2/sentinel/production/NorwAREA/netCDFNBS_work/prod_tmp/logs/ppi
 
-export PYTHONPATH=/home/elodief/monitoring_colhub:$PYTHONPATH
+export PYTHONPATH=/home/nbs/colhub:$PYTHONPATH
 source /modules/centos7/conda/Feb2021/etc/profile.d/conda.sh
 conda activate production-04-2021
 
@@ -26,25 +26,25 @@ conda activate production-04-2021
 d1=$(date -d '-1 day' +%Y%m%d)
 d2=$(date -d '-1 day' +%Y%m%d)
 
-logdir=/home/elodief/monitoring_colhub/logs
-monitdir=/lustre/storeB/project/NBS2/sentinel/production/NorwAREA/netCDFNBS_work/prod_tmp/monitoring/elodief
+logdir=/lustre/storeB/project/NBS2/sentinel/production/NorwAREA/netCDFNBS_work/prod_tmp/logs/colhub
+monitdir=/lustre/storeB/project/NBS2/sentinel/production/NorwAREA/netCDFNBS_work/prod_tmp/monitoring/dhus_logs
 
 # Check dhus instances logs
 
 # -- FRONTENDS --
-python /home/elodief/monitoring_colhub/script/monitoring_logs_dhus -d1 $d1 -d2 $d2 -dl /lustre/storeB/project/ESAcolhub/production-frontend-global/logs/NBS -o ${monitdir} -sl ${logdir}
-python /home/elodief/monitoring_colhub/script/monitoring_logs_dhus -d1 $d1 -d2 $d2 -dl /lustre/storeB/project/ESAcolhub/production-frontend-global/logs/ESA -o ${monitdir} -sl ${logdir}
-python /home/elodief/monitoring_colhub/script/monitoring_logs_dhus -d1 $d1 -d2 $d2 -dl /lustre/storeB/project/ESAcolhub/production-frontend-AOI/logs/NBS -o ${monitdir} -sl ${logdir}
+python /home/nbs/colhub/script/monitoring_logs_dhus -d1 $d1 -d2 $d2 -dl /lustre/storeB/project/ESAcolhub/production-frontend-global/logs/NBS -o ${monitdir} -sl ${logdir}
+python /home/nbs/colhub/script/monitoring_logs_dhus -d1 $d1 -d2 $d2 -dl /lustre/storeB/project/ESAcolhub/production-frontend-global/logs/ESA -o ${monitdir} -sl ${logdir}
+python /home/nbs/colhub/script/monitoring_logs_dhus -d1 $d1 -d2 $d2 -dl /lustre/storeB/project/ESAcolhub/production-frontend-AOI/logs/NBS -o ${monitdir} -sl ${logdir}
 
 # -- BACKENDS --
 area='global'
 prods='S1 S2L1C S2L2A S2DEM S3 S5p'
 for prod in $prods; do
-    python /home/elodief/monitoring_colhub/script/monitoring_logs_dhus -d1 $d1 -d2 $d2 -dl /lustre/storeB/project/ESAcolhub/production-backend-${area}/${prod}/logs -o ${monitdir} -sl ${logdir}
+    python /home/nbs/colhub/script/monitoring_logs_dhus -d1 $d1 -d2 $d2 -dl /lustre/storeB/project/ESAcolhub/production-backend-${area}/${prod}/logs -o ${monitdir} -sl ${logdir}
 done
 
 area='AOI'
 prods='S1 S2L1C S2L2A S3 S5p'
 for prod in $prods; do
-    python /home/elodief/monitoring_colhub/script/monitoring_logs_dhus -d1 $d1 -d2 $d2 -dl /lustre/storeB/project/ESAcolhub/production-backend-${area}/${prod}/logs -o ${monitdir} -sl ${logdir}
+    python /home/nbs/colhub/script/monitoring_logs_dhus -d1 $d1 -d2 $d2 -dl /lustre/storeB/project/ESAcolhub/production-backend-${area}/${prod}/logs -o ${monitdir} -sl ${logdir}
 done
