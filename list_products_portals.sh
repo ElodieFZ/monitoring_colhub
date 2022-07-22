d9d7e9fd97aa1994b3cda5d7ac58485d1381ff86
nbs <elodief@met.no>
2022-07-22 08:20:14 +0000

    Output file as argument

--- list_products_portals.sh
+++ list_products_portals.sh
@@ -25,26 +25,25 @@ set -x
 year=2020
 
 d1=${year}0101
-d2=${year}1231
+d2=${year}0102
 
 #d1=20180101
 #d2=20220615
 
 #monitdir=/lustre/storeB/project/NBS2/sentinel/production/NorwAREA/netCDFNBS_work/production/monitoring/data_gaps_barkebille/updated
-monitdir=/lustre/storeB/project/NBS2/sentinel/production/NorwAREA/netCDFNBS_work/data_gaps/data_gaps_20220609
-logdir=$monitdir/logs
+monitdir="/lustre/storeB/project/NBS2/sentinel/production/NorwAREA/netCDFNBS_work/data_gaps/data_gaps_20220609/test"
+logdir="${monitdir}/logs"
 
 hubs='scihub colhub_AOI'
+hubs='colhub_AOI'
 products='S2L1C S1 S2L2A S3'
 area='colhub_aoi'
 for hub in $hubs; do
   for p in $products; do
-    mkdir $monitdir/$year/$p
-    if [[ "$hub" == "scihub" && "$p" == "S5p" ]]; then
-      python /home/nbs/colhub/script/request_datahub -p ${p} -d1 $d1 -d2 $d2 -wl True -ld ${monitdir}/$year/$p -dh s5phub -l ${logdir} -a ${area} -wn False
-    else
-      python /home/nbs/colhub/script/request_datahub -p ${p} -d1 $d1 -d2 $d2 -wl True -ld ${monitdir}/$year/$p -dh $hub -l ${logdir} -a ${area} -wn False
-    fi
+    mkdir -p $monitdir/$year/$p
+    list_file=${monitdir}/$year/$p/products_in_${hub}_area_${area}.csv
+    [[ "$hub" == "scihub" && "$p" == "S5p" ]] && hub='S5phub'
+    python /home/nbs/colhub/script/request_datahub -p ${p} -d1 $d1 -d2 $d2 -wl True -ol $list_file -dh $hub -l ${logdir} -a ${area} -wn False
   done
 done
 
